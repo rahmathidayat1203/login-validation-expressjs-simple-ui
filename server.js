7efae712d0708d20a8cfd0a0230303bf6d745f11
rahmathidayat1203 <rh63800@gmail.com>
2022-09-03 03:11:07 +0700

    Added my project

---
+++ server.js
@@ -0,0 +1,52 @@
+/** @format */
+
+const express = require("express");
+const mysql = require("mysql");
+const BodyParser = require("body-parser");
+const md5 = require("md5");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+const db = mysql.createConnection({
+	host: "192.168.42.227",
+	database: "chat_apps",
+	user: "wsl",
+	password: "031299",
+});
+
+db.connect((err) => {
+	if (err) throw err;
+	console.log("Database Connected Successfully....🔥");
+
+	app.get("/", (req, res) => {
+		res.render("index", {
+			titleApp: "SIMPLE CHATTING APP 😊",
+			headTitle: "LOGIN FOR START CHATTING 🚪",
+		});
+	});
+
+	app.post("/login", (req, res) => {
+		const email = req.body.email;
+		const password = req.body.password;
+		const query = `SELECT * FROM users WHERE email = '${email}' AND password= '${md5(
+			password
+		)}'`;
+		db.query(query, (err, res) => {
+			if (err) throw err;
+			console.log("data same");
+		});
+		console.log(email);
+		console.log(password);
+		res.redirect("/");
+	});
+});
+
+app.listen(3001, () => {
+	console.log("------------------------");
+	console.log("server is ready now ! 🔥");
+	console.log("------------------------");
+});
